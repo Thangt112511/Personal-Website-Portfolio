ac6f6d8ad3fdec4429d6a3ec8331eeff2a3909e0
Thang Tran <Thangt112511@gmail.com>
2025-10-02 04:12:04 -0400

    resume updated v7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,10 +48,7 @@ export default function RootLayout({children,}: Readonly<{
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-	<Analytics }
-            return event;  
-          }}
-	 />
+	<Analytics/>
       </body>
     </html>
   );
